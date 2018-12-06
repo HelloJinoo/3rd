603c87b13994a969164181cbe40cd7a256ace33f
HelloJinoo <wkdwlsdn9637@naver.com>
2018-12-06 18:26:42 +0900

    al수정

--- Al/Matrixchainmultiplication/MatrixChainMultiplication.java
+++ Al/Matrixchainmultiplication/MatrixChainMultiplication.java
@@ -11,14 +11,14 @@ public class MatrixChainMultiplication {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input="";
-		
+		System.out.println("-----------행렬의 크기 입력--------");
 		while(	!(input=br.readLine()).equals("0") ) {
 			String sp[] = input.split("=");
 			String val[] = sp[1].split("x");
 			value v = new value(sp[0],Integer.parseInt(val[0]), Integer.parseInt(val[1]));
 			a.add(v);
 		}
-		int m[][] = new int[a.size()][a.size()];
+		int m[][] = new int[a.size()][a.size()]; 
 		int s[][] = new int[a.size()][a.size()];
 		
 	int n = m.length;
@@ -27,11 +27,6 @@ public class MatrixChainMultiplication {
 				int j = i+l;
 				m[i][j]= Integer.MAX_VALUE;
 				for(int k=i; k<j; k++) {
-					int b = m[i][k];
-					int c = m[k+1][j];
-					int d = a.get(i).p;
-					int f = a.get(k).q;
-					int e = a.get(j).q;
 					int q = m[i][k] + m[k+1][j] + a.get(i).p * a.get(k).q* a.get(j).q;
 					if( q < m[i][j]) {
 						m[i][j] = q;
@@ -43,11 +38,11 @@ public class MatrixChainMultiplication {
 		
 		for(int i=0; i<m.length;i++) {
 			for(int j=0; j<m[i].length; j++) {
-				System.out.printf("%8d",m[i][j] );
+				System.out.printf("%6d",m[i][j] );
 			}
 			System.out.println();
 		}
-		System.out.println("-----------------------");
+		System.out.println("---------------------------------");
 
 		for(int i=0; i<m.length;i++) {
 			for(int j=0; j<m[i].length; j++) {
@@ -55,9 +50,12 @@ public class MatrixChainMultiplication {
 			}
 			System.out.println();
 		}
-		
+		System.out.print("Optiaml solution : ");
+		System.out.println(m[0][m.length-1]);
+		System.out.print("Optimal parens : ");
 		print(s, 0,s.length-1);
 	}
+	
 	public static void print(int s[][],int i, int j) {
 		if(i == j ) {
 			System.out.print(a.get(i).a);
@@ -69,6 +67,8 @@ public class MatrixChainMultiplication {
 			System.out.print(")");
 		}
 	}
+	
+	
 	static public class value{
 		private int p;
 		private int q;
